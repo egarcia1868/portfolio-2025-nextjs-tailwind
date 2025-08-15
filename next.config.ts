275eb242c928e46b80d1984200f9b7181a3f99d1
Eric Garcia <egarcia1868@gmail.com>
2025-08-15 14:05:58 -0500

    trying to get site live

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export', // static HTML export
+  images: { unoptimized: true }, // so Next/Image works without the image optimizer
+  // If deploying to a subpath (like github.com/user/repo), set:
+  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
